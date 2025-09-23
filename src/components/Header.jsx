d857566d516aa812cbe57700e3d49e63cb16d454
Albinka <a00.ismailova@gmail.com>
2025-09-23 18:41:29 +0600

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from '../../public/logo-name.svg'
+
+export default function Header () {
+  const now = new Date()
+  return (
+    <header>
+      <img src={logo} alt={'Result'} />
+    
+
+      <span>Время сейчас: {now.toLocaleTimeString()}</span>
+    </header>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDom from 'react-dom/client' //513 (gzipped: 320)
+
+import './index.css'
+import App from './App.jsx'
+
+const root = document.getElementById('root')
+
+ReactDom.createRoot(root).render(<App/>)
+
+
